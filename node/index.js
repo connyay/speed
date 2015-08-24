d1bb7aee331d8896b152d65958e086084d9b1957
Connor Hindley <connor@redhat.com>
2015-08-24 13:53:56 -0400

    add node speed test

---
+++ node/index.js
@@ -0,0 +1,14 @@
+'use strict';
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  return res.send('lub dub');
+});
+
+var server = app.listen(8881, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
